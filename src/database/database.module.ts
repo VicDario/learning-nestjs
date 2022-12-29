efa3bae32e7152dd73f5e0263795c038184e4838
Victor Fernandez <vfernandez@dcanje.com>
2022-12-29 17:04:51 -0300

    [Add] Create connection

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,15 +1,32 @@
 import { Global, Module } from '@nestjs/common';
 
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { ConfigType } from '@nestjs/config';
+import config from 'src/config';
+import { mongo } from 'mongoose';
+
 const API_KEY = 'HelloIAmASecret';
 
 @Global()
 @Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      inject: [config.KEY],
+      useFactory: async (configService: ConfigType<typeof config>) => {
+        const { mongoUrl } = configService;
+        return {
+          uri: mongoUrl,
+        };
+      },
+    }),
+  ],
   providers: [
     {
       provide: 'API_KEY',
       useValue: API_KEY,
     },
   ],
-  exports: ['API_KEY'],
+  exports: ['API_KEY', MongooseModule],
 })
 export class DatabaseModule {}
