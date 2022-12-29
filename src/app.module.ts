687fd73f17562531c1e5dd64063dc7516707f86b
Victor Fernandez <vfernandez@dcanje.com>
2022-12-29 14:28:56 -0300

    Using global module and set config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { HttpModule, HttpService } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
 
 import { BrandsController } from './controllers/brands/brands.controller';
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 import { firstValueFrom } from 'rxjs';
-
-const API_KEY = 'HelloIAmASecret';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
     HttpModule.registerAsync({
       useFactory: () => ({
         timeout: 1000,
@@ -20,26 +24,25 @@ const API_KEY = 'HelloIAmASecret';
       }),
     }),
     ProductsModule,
-    UsersModule
+    UsersModule,
+    DatabaseModule,
   ],
   controllers: [AppController, BrandsController],
   providers: [
     AppService,
-    {
-      provide: 'API_KEY',
-      useValue: API_KEY,
-    },
+
     {
       provide: 'TASKS',
       inject: [HttpService],
       useFactory: async (http: HttpService) => {
-        const tasks = http.get('https://jsonplaceholder.typicode.com/todos', { // Calling an API only for educational purpose
-          headers: { 'Accept-Encoding': 'gzip,deflate,compress' }
+        // Calling an API only for educational purpose
+        const tasks = http.get('https://jsonplaceholder.typicode.com/todos', {
+          headers: { 'Accept-Encoding': 'gzip,deflate,compress' },
         });
         const data = await firstValueFrom(tasks);
-        return data.data
+        return data.data;
       },
-    }
+    },
   ],
 })
 export class AppModule {}
