1f09c49fe7ecf28cf724ab8dc8571ddc46162478
Victor Fernandez <vfernandez@dcanje.com>
2022-12-29 12:25:05 -0300

    Test useValue

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,13 +6,19 @@ import { BrandsController } from './controllers/brands/brands.controller';
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 
-
+const API_KEY = 'HelloIAmASecret';
 @Module({
   imports: [ProductsModule, UsersModule],
   controllers: [
     AppController,
     BrandsController
   ],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'API_KEY',
+      useValue: API_KEY
+    }
+  ],
 })
 export class AppModule {}
