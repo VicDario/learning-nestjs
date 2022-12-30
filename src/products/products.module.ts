15ad2c5ac07acbe1f18a05b054a3fbfe0696a248
Victor Fernandez <vfernandez@dcanje.com>
2022-12-30 10:35:15 -0300

    [Change] Import mongo and schema

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
 
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { ProductsController } from './controllers/products/products.controller';
 import { CategoriesController } from './controllers/categories/categories.controller';
 
 import { ProductsService } from './services/products/products.service';
 
+import { Product, ProductSchema } from './entities/product.entity';
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Product.name,
+        schema: ProductSchema,
+      },
+    ]),
+  ],
   controllers: [ProductsController, CategoriesController],
   providers: [ProductsService],
   exports: [ProductsService],
