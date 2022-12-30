aa3041387ac522a5b0ed51ac3aee8c6c9bdda44c
Victor Fernandez <vfernandez@dcanje.com>
2022-12-30 10:41:09 -0300

    [Add] MongoID pipe

---
+++ src/common/pipes/mongo-id/mongo-id.pipe.ts
@@ -0,0 +1,17 @@
+import {
+  ArgumentMetadata,
+  Injectable,
+  PipeTransform,
+  BadRequestException,
+} from '@nestjs/common';
+
+import { isMongoId } from 'class-validator';
+
+@Injectable()
+export class MongoIdPipe implements PipeTransform {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  transform(value: string, _metadata: ArgumentMetadata) {
+    if (!isMongoId(value)) throw new BadRequestException('Invalid ID');
+    return value;
+  }
+}
