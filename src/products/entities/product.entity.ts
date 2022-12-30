f5401c4e25e517d68c53681c37f1d6a69b2b350f
Victor Fernandez <vfernandez@dcanje.com>
2022-12-30 10:31:37 -0300

    [Change] Entity now is a mongo schema

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,6 +1,40 @@
-export class Product {
-  id: number;
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class Product extends Document {
+  @Prop({ unique: true, required: true })
+  sku: string;
+
+  @Prop({ required: true })
   name: string;
+
+  @Prop({ required: true })
+  brand: string;
+
+  @Prop({ required: true })
   description: string;
+
+  @Prop({
+    type: Number,
+    required: true,
+    validators: [
+      {
+        validator: Number.isInteger,
+        message: 'Stock only can be an integer value',
+      },
+      {
+        validator: (value: number) => {
+          value >= 0;
+        },
+        message: 'Stock cannot be lower than 0',
+      },
+    ],
+  })
+  stock: number;
+
+  @Prop({ type: Number, required: true })
   price: number;
 }
+
+export const ProductSchema = SchemaFactory.createForClass(Product);
