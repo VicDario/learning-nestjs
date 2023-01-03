e93e7007bd1b64b1b40241a2568314447702a91a
Victor Fernandez <vfernandez@dcanje.com>
2023-01-02 23:20:42 -0300

    [Add] Using new filters

--- src/products/services/products/products.service.ts
+++ src/products/services/products/products.service.ts
@@ -1,9 +1,13 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, FilterQuery } from 'mongoose';
 
 import { Product } from 'src/products/entities/product.entity';
-import { ProductDto, UpdateProductDto } from 'src/products/dtos/product.dto';
+import {
+  FilterProductsDto,
+  ProductDto,
+  UpdateProductDto,
+} from 'src/products/dtos/product.dto';
 
 @Injectable()
 export class ProductsService {
@@ -11,7 +15,18 @@ export class ProductsService {
     @InjectModel(Product.name) private productModel: Model<Product>,
   ) {}
 
-  async findAll() {
+  async findAll(params?: FilterProductsDto) {
+    if (params) {
+      const filters: FilterQuery<Product> = {}
+      const { limit, offset, minPrice, maxPrice } = params;
+      if (minPrice && maxPrice) {
+        filters.price = {
+          $gte: minPrice,
+          $lte: maxPrice
+        }
+      }
+      return await this.productModel.find(filters).skip(offset).limit(limit).exec();
+    }
     return await this.productModel.find().exec();
   }
 
